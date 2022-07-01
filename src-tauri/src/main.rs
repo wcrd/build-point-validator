1e6dd2a72deff61ecdc34ccbd6b443497ab7c0aa
wcrd <11898748+wcrd@users.noreply.github.com>
2022-07-01 12:53:16 -0600

    Update tauri versions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,8 +8,8 @@ use tauri::{CustomMenuItem, Menu, Submenu};
 
 fn main() {
   // Define menu
-  let version = CustomMenuItem::new("_version_", "v1.0.0").disabled();
-  let about = CustomMenuItem::new("_about_", "wcrd 2021").disabled();
+  let version = CustomMenuItem::new("_version_", "v1.1.0").disabled();
+  let about = CustomMenuItem::new("_about_", "wcrd 2022").disabled();
   let load_ref = CustomMenuItem::new("update_point_ref".to_string(), "Update point reference");
   let file_menu = Submenu::new("File", Menu::new().add_item(load_ref));
   let about_menu = Submenu::new("About", Menu::new().add_item(version).add_item(about));
