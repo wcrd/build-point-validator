bbb047ac07c110254269b9632a17cbe4e794a28a
wcrd <11898748+wcrd@users.noreply.github.com>
2022-07-14 20:11:44 +0200

    - Add Quit menu item - Add Reload item placeholder

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,7 +6,7 @@
 // extern crate dotenv;
 // use dotenv::dotenv;
 use std::env;
-use tauri::{CustomMenuItem, Menu, Submenu};
+use tauri::{CustomMenuItem, Menu, Submenu, MenuItem};
 
 const APP_VERSION: &'static str = env!("APP_VERSION");
 
@@ -16,7 +16,8 @@ fn main() {
   let version = CustomMenuItem::new("_version_", format!("v{}", APP_VERSION )).disabled();
   let about = CustomMenuItem::new("_about_", "wcrd 2022").disabled();
   let load_ref = CustomMenuItem::new("update_point_ref".to_string(), "Update point reference");
-  let file_menu = Submenu::new("File", Menu::new().add_item(load_ref));
+  let reload = CustomMenuItem::new("reload-app", "Reload");
+  let file_menu = Submenu::new("File", Menu::new().add_item(load_ref).add_item(reload).add_native_item(MenuItem::Quit));
   let about_menu = Submenu::new("About", Menu::new().add_item(version).add_item(about));
   let menu = Menu::new()
     .add_submenu(file_menu)
